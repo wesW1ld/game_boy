6bc44abf6c026c807f6580aad4b2181d05d041e1
Wesley Wild <wdw0510@gmail.com>
2025-05-11 19:50:16 -0400

    3 loads

--- CPU.cpp
+++ CPU.cpp
@@ -26,7 +26,61 @@ void CPU::start()
     std::cout << "CPU started.";
 }
 
+//load instructions
+void CPU::LD_r8_r8(int dest, int src)
+{
+    registers[dest] = registers[src];
+}
+void CPU::LD_r8_n8(int dest, uint8_t val)
+{
+    registers[dest] = val;
+}
+void CPU::LD_r16_n16(int dest, uint16_t val)
+{
+    if(dest % 2 == 0)
+    {
+        registers[dest] = static_cast<uint8_t>(val>>8);
+        registers[dest + 1] = static_cast<uint8_t>(val&0xFF);
+    }
+    else
+    {
+        std::cout << "Ivalid 16 bit address" << std::endl;
+    }
+}
+void CPU::LD_memHL_r8(int src)
+{
+
+}
+void CPU::LD_memHL_n8(uint8_t value)
+{
+
+}
+void CPU::LD_r8_memHL(int dest)
+{
+
+}
+void CPU::LD_mem_r16_A(uint16_t& dest);
+void CPU::LD_mem_n16_A(uint16_t dest);
+void CPU::LDH_mem_n16_A(uint16_t dest);
+void CPU::LDH_mem_C_A();
+void CPU::LD_A_mem_r16(uint16_t& src);
+void CPU::LD_A_mem_n16(uint16_t src);
+void CPU::LDH_A_mem_n16(uint8_t src);
+void CPU::LDH_A_mem_C();
+void CPU::LD_mem_HLI_A();
+void CPU::LD_mem_HLD_A();
+void CPU::LD_A_mem_HLI();
+void CPU::LD_A_mem_HLD();
+
 uint16_t CPU::getPair(int firstAdress) //TODO: add check to make sure pair is valid
 {
-    return (registers[firstAdress] << 8) | (registers[firstAdress + 1]);
+    if(firstAdress % 2 == 0)
+    {
+        return (registers[firstAdress] << 8) | (registers[firstAdress + 1]);
+    }
+    else
+    {
+        std::cout << "Ivalid 16 bit address" << std::endl;
+        return 0;
+    }
 }
--- CPU.hpp
+++ CPU.hpp
@@ -11,27 +11,31 @@ class CPU
         uint16_t SP; // Stack Pointer
         const char RegName[8][6];
 
-        uint8_t* memory; //pointer to memory which is stored outside of the CPU
+        uint16_t* memory; //pointer to memory which is stored outside of the CPU
 
         CPU();
 
         void start();
 
         //load instructions
-        void LD(int reg1, int reg2);
-        void LDi(int reg, int value);
-        void LD16(int reg1, int reg2);
-        void LDtoM(int address, int reg);//load to memory
-        void LDM(int reg, int address);//load memory
-        void LD16toM(int reg1, int reg2);//copy from reg2 to address at 16 bit reg1
-        void LD16toMi(int value, int reg);//value is address
-        void LDHi(int reg, int value);//swap above
-        void LDHc(int c, int reg);//reg to $FF00+C
-        void LD();
-        void LD();
-        void LDHM(int reg1, int reg2);//at address from reg2 (16bit) to reg1
-        void LDHMc(int reg, int c);//$FF00+C to reg
-        //4 more, rest of load are stack stuff for later
+        void LD_r8_r8(int dest, int src);                             // LD r8, r8
+        void LD_r8_n8(int dest, uint8_t val);                         // LD r8, n8
+        void LD_r16_n16(int dest, uint16_t val);                      // LD r16, n16
+        void LD_memHL_r8(int src);                                    // LD [HL], r8
+        void LD_memHL_n8(uint8_t value);                              // LD [HL], n8
+        void LD_r8_memHL(int dest);                                   // LD r8, [HL]
+        void LD_mem_r16_A(uint16_t& dest);                            // LD [r16], A
+        void LD_mem_n16_A(uint16_t dest);                             // LD [n16], A
+        void LDH_mem_n16_A(uint16_t dest);                            // LDH [n16], A
+        void LDH_mem_C_A();                                           // LDH [C], A
+        void LD_A_mem_r16(uint16_t& src);                             // LD A, [r16]
+        void LD_A_mem_n16(uint16_t src);                              // LD A, [n16]
+        void LDH_A_mem_n16(uint8_t src);                              // LDH A, [n16]
+        void LDH_A_mem_C();                                           // LDH A, [C]
+        void LD_mem_HLI_A();                                          // LD [HLI], A (post-increment)
+        void LD_mem_HLD_A();                                          // LD [HLD], A (post-decrement)
+        void LD_A_mem_HLI();                                          // LD A, [HLI]
+        void LD_A_mem_HLD();                                          // LD A, [HLD]
 
     private:
     uint16_t getPair(int firstAdress);
